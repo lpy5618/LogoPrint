df2f5b05c06244ed2191a9c422f4ac4f143ec817
lpy5618 <lpy547898@gmail.com>
2023-05-24 17:59:15 +1000

    upload resources

--- main.py
+++ main.py
@@ -19,45 +19,54 @@ def choosePosition(windowName,img):
     key=cv.waitKey(0)
     if key==13:           #Enter
         del temp_img
-        cv.destroyAllWindows
+        cv.destroyAllWindows()
         return points
     elif key==27:           #ESC
         del temp_img
-        cv.destroyAllWindows
+        cv.destroyAllWindows()
         return
     else:
         return choosePosition(windowName,img)
 
 
-#background image
-backgroundPicPath=easygui.fileopenbox(msg="please choose the background image")
-backgroundPic=Image.open(backgroundPicPath)
+index=1
+while(index):
+    #background image
+    backgroundPicPath=easygui.fileopenbox(msg="please choose the background image")
+    backgroundPic=Image.open(backgroundPicPath)
 
-#overlay image
-logoPicPath=easygui.fileopenbox(msg="please choose the logo")
-logoPic=Image.open(logoPicPath)
+    #overlay image
+    logoPicPath=easygui.fileopenbox(msg="please choose the logo")
+    logoPic=Image.open(logoPicPath)
 
-#logo reshape
-width,height=logoPic.size
-rate=int(input("please input the size of the logo: "))
-newWidth=int(width*rate/100)
-newHeight=int(height*rate/100)
-logoPic=logoPic.resize((newWidth,newHeight))
+    #logo reshape
+    width,height=logoPic.size
+    rate=int(input("please input the size of the logo: "))
+    newWidth=int(width*rate/100)
+    newHeight=int(height*rate/100)
+    logoPic=logoPic.resize((newWidth,newHeight))
 
-#input the logo position
-# positionInput=map(int,input("please input the position of the logo: ").split(","))
-# position=tuple(positionInput)
+    choice=input("which method do you prefer?\n1.input the logo position   2.pick a position by mouse\n")
+    if choice=='1':
+    #input the logo position
+        positionInput=map(int,input("please input the position of the logo: ").split(","))
+        position=tuple(positionInput)
+    elif choice=='2':
+    #pick the logo position by mouse
+        windowName="Position Picker"
+        position=choosePosition(windowName,backgroundPic)[0]
+    else:
+        print("wrong option, try again")
+        continue
 
-#pick the logo position by mouse
-windowName="Position Picker"
-position=choosePosition(windowName,backgroundPic)[0]
+    #paste logo to background pic
+    backgroundPic.paste(logoPic,position)
 
-#paste logo to background pic
-backgroundPic.paste(logoPic,position)
+    backgroundPic.show()
 
-backgroundPic.show()
-
-#save result pic to file
-savePath=easygui.filesavebox(msg="where do you want to save this file? ",default="test.png" )
-if savePath:
-    backgroundPic.save(savePath)
+    #save result pic to file
+    savePath=easygui.filesavebox(msg="where do you want to save this file? ",default="test.png" )
+    if savePath:
+        backgroundPic.save(savePath)
+        print("result pic saved at "+savePath)
+    index=int(input("do you wish to continue? input 0 to exit\n"))
