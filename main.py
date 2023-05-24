a99ef1201f89ba91d0456a6bd6509ca4d845a6bc
lpy5618 <lpy547898@gmail.com>
2023-05-24 13:16:22 +1000

    pick up(GUI) resize save
    allow user pickup files by GUI, resize the logo, save the result and choose the path for saving

--- main.py
+++ main.py
@@ -11,9 +11,17 @@ logoPicPath=easygui.fileopenbox()
 img2=Image.open(logoPicPath)
 
 #logo reshape
-img2=img2.resize((270,310))
+width,height=img2.size
+rate=int(input("please input the size of the logo: "))
+newWidth=int(width*rate/100)
+newHeight=int(height*rate/100)
+img2=img2.resize((newWidth,newHeight))
 
 #paste img2 to img1
 img1.paste(img2,(357,762))
 
 img1.show()
+
+#save result pic to file
+savePath=easygui.filesavebox(msg="where do you want to save this file? ",default="test.png" )
+img1.save(savePath)
