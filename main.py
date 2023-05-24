04609c3c56ef3d52d1bef4d93c1bbe1c1d923657
lpy5618 <lpy547898@gmail.com>
2023-05-24 15:57:45 +1000

    T7: pick logo position by mouse

--- main.py
+++ main.py
@@ -1,30 +1,63 @@
 from PIL import Image
 import easygui
+import cv2 as cv
+import numpy as np
+
+def choosePosition(windowName,img):
+    points=[]
+    img=np.array(img)
+    def onMouse(event,x,y,flags,param):
+        if event==cv.EVENT_LBUTTONDOWN:
+            cv.circle(temp_img,(x,y),5,(102,217,239),-1)
+            points.append((x,y))
+            cv.imshow(windowName,temp_img)
+    
+    temp_img=img.copy()
+    cv.namedWindow(windowName)
+    cv.imshow(windowName,temp_img)
+    cv.setMouseCallback(windowName,onMouse)
+    key=cv.waitKey(0)
+    if key==13:           #Enter
+        del temp_img
+        cv.destroyAllWindows
+        return points
+    elif key==27:           #ESC
+        del temp_img
+        cv.destroyAllWindows
+        return
+    else:
+        return choosePosition(windowName,img)
 
 
 #background image
 backgroundPicPath=easygui.fileopenbox(msg="please choose the background image")
-img1=Image.open(backgroundPicPath)
+backgroundPic=Image.open(backgroundPicPath)
 
 #overlay image
 logoPicPath=easygui.fileopenbox(msg="please choose the logo")
-img2=Image.open(logoPicPath)
+logoPic=Image.open(logoPicPath)
 
 #logo reshape
-width,height=img2.size
+width,height=logoPic.size
 rate=int(input("please input the size of the logo: "))
 newWidth=int(width*rate/100)
 newHeight=int(height*rate/100)
-img2=img2.resize((newWidth,newHeight))
+logoPic=logoPic.resize((newWidth,newHeight))
 
-#paste img2 to img1
-positionInput=map(int,input("please input the position of the logo: ").split(","))
-position=tuple(positionInput)
-img1.paste(img2,position)
+#input the logo position
+# positionInput=map(int,input("please input the position of the logo: ").split(","))
+# position=tuple(positionInput)
 
-img1.show()
+#pick the logo position by mouse
+windowName="Position Picker"
+position=choosePosition(windowName,backgroundPic)[0]
+
+#paste logo to background pic
+backgroundPic.paste(logoPic,position)
+
+backgroundPic.show()
 
 #save result pic to file
 savePath=easygui.filesavebox(msg="where do you want to save this file? ",default="test.png" )
 if savePath:
-    img1.save(savePath)
+    backgroundPic.save(savePath)
