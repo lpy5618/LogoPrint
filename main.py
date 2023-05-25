8dad9bb5bd59140621c70a21e04009efa9f8b02e
lpy5618 <lpy547898@gmail.com>
2023-05-25 12:42:45 +1000

    T9, T10 random pick and save files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image
 import easygui
 import cv2 as cv
 import numpy as np
+from randomPick import randomPickPics
 
 def choosePosition(windowName,img):
     points=[]
@@ -31,10 +32,13 @@ def choosePosition(windowName,img):
 
 index=1
 while(index):
-    #background image
-    backgroundPicPath=easygui.fileopenbox(msg="please choose the background image")
-    backgroundPic=Image.open(backgroundPicPath)
-
+    backgroundPicFolder=easygui.diropenbox(msg="please choose the background image folder")
+    fileNum=int(input("how many pics do you want to get?"))
+    print(fileNum)
+    print(backgroundPicFolder)
+    backgroundPicPaths=randomPickPics(backgroundPicFolder,fileNum)
+    print(backgroundPicPaths)
+    
     #overlay image
     logoPicPath=easygui.fileopenbox(msg="please choose the logo")
     logoPic=Image.open(logoPicPath)
@@ -46,31 +50,43 @@ while(index):
     newHeight=int(height*rate/100)
     logoPic=logoPic.resize((newWidth,newHeight))
 
-    choice=input("which method do you prefer?\n1.input the logo position   2.pick a position by mouse   3.central point of background pic\n")
-    if choice=='1':
-    #input the logo position
-        positionInput=map(int,input("please input the position of the logo: ").split(","))
-        position=tuple(positionInput)
-    elif choice=='2':
-    #pick the logo position by mouse
-        windowName="Position Picker"
-        position=choosePosition(windowName,backgroundPic)[0]
-    elif choice=='3':
+    
+
+    # choice=input("which method do you prefer?\n1.input the logo position   2.pick a position by mouse   3.central point of background pic\n")
+    # if choice=='1':
+    # #input the logo position
+    #     positionInput=map(int,input("please input the position of the logo: ").split(","))
+    #     position=tuple(positionInput)
+    # elif choice=='2':
+    # #pick the logo position by mouse
+    #     windowName="Position Picker"
+    #     position=choosePosition(windowName,backgroundPic)[0]
+    # elif choice=='3':
+    # #paste logo at central point
+    #     centralPoint=[backgroundPic.size[0]/2,backgroundPic.size[1]/2]#central point of the background pic
+    #     position=tuple((int(centralPoint[0]-newWidth/2),int(centralPoint[1]-newHeight/2)))
+    # else:
+    #     print("wrong option, try again")
+    #     continue
+    saveFolder=easygui.diropenbox(msg="please choose the result saving folder")
+
+    for i in range(fileNum):
+        #background image
+        backgroundPicPath=backgroundPicPaths.pop()
+        backgroundPic=Image.open(backgroundPicPath)
         centralPoint=[backgroundPic.size[0]/2,backgroundPic.size[1]/2]#central point of the background pic
         position=tuple((int(centralPoint[0]-newWidth/2),int(centralPoint[1]-newHeight/2)))
-    else:
-        print("wrong option, try again")
-        continue
 
-    #paste logo to background pic
-    backgroundPic.paste(logoPic,position)
+        #paste logo to background pic
+        backgroundPic.paste(logoPic,position)
 
     #show the result pic
-    backgroundPic.show()
+        # backgroundPic.show()
 
     #save result pic to file
-    savePath=easygui.filesavebox(msg="where do you want to save this file? ",default="test.png" )
-    if savePath:
-        backgroundPic.save(savePath)
-        print("result pic saved at "+savePath)
+    
+        savePath=saveFolder+"\\"+backgroundPicPath.split("/").pop(-1)
+        if savePath:
+            backgroundPic.save(savePath)
+            print("result pic saved at "+savePath)
     index=int(input("do you wish to continue? input 0 to exit\n"))
---
+++ randomPick.py
@@ -0,0 +1,30 @@
+import random
+import os
+
+def randomPickPics(folderPath,fileNum):
+    filePaths=[]
+    files=os.listdir(folderPath)
+    
+    totalNum=len(files)
+    
+    randomNum=[]
+    while(1):
+        ran=random.randint(0,totalNum-1)
+        if ran in randomNum:
+            continue
+        else:
+            randomNum.append(ran)
+        if len(randomNum)==fileNum:
+            break
+    for i in range(fileNum):
+        filePath=folderPath+"/"+str(randomNum.pop())+".png"
+        filePaths.append(filePath)
+    return filePaths
+
+
+# folderpath='H://LogoPrint//LogoPrint//pictures//bag//'
+# filenum=10
+
+# filepaths=randomPickPics(folderpath,filenum)
+# for i in range(filenum):
+#     print(filepaths.pop())
