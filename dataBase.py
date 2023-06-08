eae38336b97337f8b749ea47200fade83d554177
lpy5618 <lpy547898@gmail.com>
2023-06-08 11:10:28 +1000

    bug fix: system get all data from database if user input nothing

--- dataBase.py
+++ dataBase.py
@@ -24,18 +24,26 @@ def getPathFromDB():
     query = {}
     if args['color'] != '':
         query['ColourPart1Des'] = re.compile(args['color'],re.IGNORECASE)
-    if args['category'] != '':
+    elif args['category'] != '':
         query['category'] = re.compile(args['category'],re.IGNORECASE)
-    if args['printtype'] != '':
+    elif args['printtype'] != '':
         query['printtype'] = re.compile(args['printtype'],re.IGNORECASE)
+    else:
+        pass
     # if command[0]=="bottles":
     #     pathCursor=bottlesCol.find(cmd,{"_id":0,"imagePath":1})
     # elif command[0]=="bags":
     #     pathCursor=bagsCol.find(cmd,{"_id":0,"imagePath":1})
     projection = {"_id": 0, "imagePath": 1}
-    pathCursor=productsCol.find(query, projection)
+    if len(query)==0:
+        pathCursor=productsCol.find({}, projection)
+    else:
+        pathCursor=productsCol.find(query, projection)
     for i in pathCursor:
-        paths.append(i["imagePath"])
+        try:
+            paths.append(i["imagePath"])
+        except:
+            continue
     if len(paths)<fileNum:
         return paths
     else:
